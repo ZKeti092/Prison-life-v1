493fe431b61cfd13ac3a74d1ab3b5cc00b14c8f5
ZKeti092 <166065191+ZKeti092@users.noreply.github.com>
2024-04-05 13:54:35 +0300

    Create Prison.lua

---
+++ Prison.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/ZKeti092/Prison-life-v1/main/Prison.lua",true))()",true))()
+
+local Window = Library.createLib("Prison life v1", "Ziyad")
+
+local tab = Window:NewTab("Player") 
